5936623cbf8956f834015a1eb8fed14a6efd927f
Gyrate <gyratesky@gmail.com>
2022-05-24 17:40:53 +0800

    canvas实现流光效果

--- src/views/FlowLight.vue
+++ src/views/FlowLight.vue
@@ -21,6 +21,11 @@
 
 <script>
 //https://segmentfault.com/a/1190000008560571
+//流光效果核心代码
+//https://blog.csdn.net/LaineGates/article/details/105745843
+//椭圆计算公式
+//https://segmentfault.com/a/1190000018597975
+//贝塞尔曲线计算公式
 export default {
   name: "FlowLight.vue",
   components: {},
@@ -45,12 +50,22 @@ export default {
 
       //椭圆轨迹
       ellipseConf: {
-        a: 100,
-        b: 50,
-        angle: 0,
-        centerX: 250,
-        centerY: 150
-      }
+        a: 100, //长半径
+        b: 70, //短半径
+        angle: 0, //弧度
+        centerX: 250, //中心点x
+        centerY: 150 //中心点y
+      },
+
+      //2阶贝塞尔
+      twoBezierConf: {
+        p1: [100,100], //起点坐标
+        p2: [400, 100],//终点坐标
+        cp: [250,200], //拐点坐标
+        t: 0,
+      },
+
+
 
     }
   },
@@ -116,21 +131,73 @@ export default {
       //   this.circleConf.angle += 0.02
       // }
 
-      //椭圆
-      // x=a∗cos(t)
-      // y=b∗sin(t)
-      const {a, b, angle, centerX, centerY } = this.ellipseConf
-      this.x = a * Math.cos(angle) + centerX
-      this.y = b * Math.sin(angle) + centerY
+      //椭圆：x=a∗cos(t) , y=b∗sin(t)
+      // const {a, b, angle, centerX, centerY } = this.ellipseConf
+      // this.x = a * Math.cos(angle) + centerX
+      // this.y = b * Math.sin(angle) + centerY
+      //
+      // if (angle > 2 * Math.PI) {
+      //   this.ellipseConf.angle = 0
+      // } else {
+      //   this.ellipseConf.angle += 0.02
+      // }
 
-      if (angle > 2 * Math.PI) {
-        this.ellipseConf.angle = 0
+      //2阶贝塞尔曲线
+      const {p1, p2, cp, t} = this.twoBezierConf
+      const [x1, y1] = this.twoBezier(t, p1, cp, p2)
+      this.x = x1
+      this.y = y1
+      if (t > 1) {
+        this.twoBezierConf.t = 0
       } else {
-        this.ellipseConf.angle += 0.02
+        this.twoBezierConf.t += 0.005
       }
 
+      //3阶段贝塞尔曲线
+      // const
 
-      //贝塞尔曲线
+    },
+
+    /**
+     * @desc 二阶贝塞尔
+     * @param {number} t 当前百分比
+     * @param {Array} p1 起点坐标
+     * @param {Array} p2 终点坐标
+     * @param {Array} cp 控制点
+     */
+    twoBezier(t, p1, cp, p2) {
+      const [x1, y1] = p1;
+      const [cx, cy] = cp;
+      const [x2, y2] = p2;
+      let x = (1 - t) * (1 - t) * x1 + 2 * t * (1 - t) * cx + t * t * x2;
+      let y = (1 - t) * (1 - t) * y1 + 2 * t * (1 - t) * cy + t * t * y2;
+      return [x, y];
+    },
+
+    /**
+     * @desc 三阶贝塞尔
+     * @param {number} t 当前百分比
+     * @param {Array} p1 起点坐标
+     * @param {Array} p2 终点坐标
+     * @param {Array} cp1 控制点1
+     * @param {Array} cp2 控制点2
+     */
+    threeBezier(t, p1, cp1, cp2, p2) {
+      const [x1, y1] = p1;
+      const [x2, y2] = p2;
+      const [cx1, cy1] = cp1;
+      const [cx2, cy2] = cp2;
+      let x =
+          x1 * (1 - t) * (1 - t) * (1 - t) +
+          3 * cx1 * t * (1 - t) * (1 - t) +
+          3 * cx2 * t * t * (1 - t) +
+          x2 * t * t * t;
+      let y =
+          y1 * (1 - t) * (1 - t) * (1 - t) +
+          3 * cy1 * t * (1 - t) * (1 - t) +
+          3 * cy2 * t * t * (1 - t) +
+          y2 * t * t * t;
+      return [x, y];
     },
 
     hexToRgba(hex, opacity) {
